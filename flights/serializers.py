d4a0a2c8223bd075fb25a9f5b2e8e93310b5e790
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-01-27 17:09:27 +0300

    REST task 1

---
+++ flights/serializers.py
@@ -0,0 +1,13 @@
+
+from .models import Flight, Booking
+from rest_framework import serializers
+
+class FlightSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Flight
+		fields = ['id', 'destination', 'time', 'price']
+
+class BookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		fields = ['flight', 'date', 'id']
--- task_1/urls.py
+++ task_1/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from flights import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/flights/', views.FlightList.as_view(),name='flights-list'),
+    path('api/booking/', views.BookingList.as_view(), name='bookings-list'),
 ]
